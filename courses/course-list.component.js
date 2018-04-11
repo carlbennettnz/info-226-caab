93a0b77b00cea8943a6072b2646fefe4dca577bf
Carl Bennett <carlbennettnz@gmail.com>
2018-04-12 10:12:29 +1200

    OCD

--- courses/course-list.component.js
+++ courses/course-list.component.js
@@ -4,9 +4,8 @@ angular
     templateUrl: 'courses/course-list.template.html',
     controller($scope) {
       $scope.courses = [
-      {code: 'INFO226', title: 'Web Application Development'},
-      {code: 'INFO6354g', title:' PFJO'}
+        { code: 'INFO226', title: 'Web Application Development' },
+        { code: 'INFO6354g', title:' PFJO' }
       ]
-
     }
   })
