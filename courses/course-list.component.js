2fabc42105e113b62822077cd1ddffb185b6a4a3
Carl Bennett <carlbennettnz@gmail.com>
2018-04-12 11:16:10 +1200

    Load courses from the server

--- courses/course-list.component.js
+++ courses/course-list.component.js
@@ -2,10 +2,22 @@ angular
   .module('courses')
   .component('courseList', {
     templateUrl: 'courses/course-list.template.html',
-    controller($scope) {
+    controller($scope, $http) {
       $scope.courses = [
         { code: 'INFO226', title: 'Web Application Development' },
         { code: 'INFO6354g', title:' PFJO' }
       ]
+
+      $http({
+        method: 'GET',
+        url: 'https://caab.sim.vuw.ac.nz/api/bennetcarl2/course_directory.json'
+      }).then(response => {
+        if (response.data.courses) {
+          $scope.courses = response.data.courses.map(course => ({
+            code: course.ID,
+            title: course.Name
+          }))
+        }
+      })
     }
   })
