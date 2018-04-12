19dd96f76c8071f34442785aeda1f1cc8253b395
Carl Bennett <carlbennettnz@gmail.com>
2018-04-12 12:20:55 +1200

    Make the login form fully functional

--- accounts/login.component.js
+++ accounts/login.component.js
@@ -2,10 +2,38 @@ angular
   .module('accounts')
   .component('login', {
     templateUrl: 'accounts/login.template.html',
-    controller($scope, $location) {
+    controller($scope, $location, $http) {
+      $scope.errors = []
+
+      const userListPromise = $http({
+        method: 'GET',
+        url: `https://caab.sim.vuw.ac.nz/api/bennetcarl/user_list.json`
+      })
+
       $scope.action = ($event) => {
         $event.preventDefault()
-        $location.path('/dashboard')
+
+        $scope.errors = []
+        if (!$scope.username) $scope.errors.push('You must provide a username')
+        if (!$scope.password) $scope.errors.push('You must provide a password')
+        if ($scope.errors.length) return
+
+        userListPromise.then(response => {
+          console.log(response)
+          const users = response.data.users
+
+          const user = users.find(
+            user => user.LoginName === $scope.username
+              && user.Password === $scope.password
+          )
+          
+          if (!user) {
+            $scope.errors.push('Login details are incorrect')
+            return
+          }
+
+          $location.path('/dashboard')
+        })
       }
     }
   })
