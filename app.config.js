0208499c58d895047b36201108d43845acf349ad
Carl Bennett <carlbennettnz@gmail.com>
2018-04-19 09:38:14 +1200

    Pull config out into a separate function

--- app.config.js
+++ app.config.js
@@ -1,18 +1,20 @@
 const application = angular
   .module('app')
-  .config(function($routeProvider) {
-    $routeProvider
-      .when('/login', {
-        templateUrl: 'accounts/login.template.html',
-        controller: 'login'
-      })
-      .when('/students/dashboard', {
-        templateUrl: 'student-dashboard.template.html',
-        controller: 'studentDashboard'
-      })
-      .when('/lecturers/dashboard', {
-        templateUrl: 'lecturer-dashboard.template.html',
-        controller: 'lecturerDashboard'
-      })
-      .otherwise('/login')
-  })
+  .config(configureApp)
+
+function configureApp($routeProvider) {
+  $routeProvider
+    .when('/login', {
+      templateUrl: 'accounts/login.template.html',
+      controller: 'login'
+    })
+    .when('/students/dashboard', {
+      templateUrl: 'student-dashboard.template.html',
+      controller: 'studentDashboard'
+    })
+    .when('/lecturers/dashboard', {
+      templateUrl: 'lecturer-dashboard.template.html',
+      controller: 'lecturerDashboard'
+    })
+    .otherwise('/login')
+}
