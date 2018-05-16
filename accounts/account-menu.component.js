72fcb07ef24a2e0e022fc8b1b61f701de626ae52
Carl Bennett <carlbennettnz@gmail.com>
2018-05-17 10:46:01 +1200

    Persist themes to server

--- accounts/account-menu.component.js
+++ accounts/account-menu.component.js
@@ -5,13 +5,14 @@ angular
     controller: accountMenuComponent
   })
 
-function accountMenuComponent($scope, session) {
+function accountMenuComponent($scope, session, store) {
   $scope.showMenu = false
   $scope.toggleMenu = () => $scope.showMenu = !$scope.showMenu
   $scope.user = session.getUser()
   $scope.getColourClass = (colour) => `colour ${colour} ${$scope.theme === colour ? 'selected' : ''}`
   $scope.selectColour = (colour) => {
     localStorage.colour = colour
+    store.save('themes', { id: $scope.user.id, colour })
     $scope.theme = colour
     document.body.style.backgroundColor = colour
   }
@@ -23,4 +24,5 @@ function accountMenuComponent($scope, session) {
   ]
   $scope.theme = localStorage.colour || $scope.colours[0]
   document.body.style.backgroundColor = $scope.theme
+  store.get('themes', $scope.user.id).then(theme => theme && theme.colour && $scope.selectColour(theme.colour))
 }
