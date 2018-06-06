35575531018639f963ebe3a7816e6f6bab456f02
Carl Bennett <carlbennettnz@gmail.com>
2018-06-07 09:01:33 +1200

    Load stats from server

--- courses/lecturer-course-card.component.js
+++ courses/lecturer-course-card.component.js
@@ -2,10 +2,28 @@ angular
   .module('courses')
   .component('lecturerCourseCard', {
     templateUrl: 'courses/lecturer-course-card.template.html',
-    component: lecturerCourseCardComponent,
+    controller: lecturerCourseCardComponent,
     bindings: { course: '<' }
   })
 
-function lecturerCourseCardComponent() {
-  console.log('hi')
+function lecturerCourseCardComponent($scope, $q, store) {
+  $scope.loading = true
+
+  store.get('assignments', a => a.courseId === this.course.id)
+    .then(assignments => assignments.map(a => a.id))
+    .then(assignmentIds => {
+      console.log(assignmentIds)
+
+      const loadSubmissions = store.get('submissions', s => assignmentIds.includes(s.assignmentId) && !s.grade)
+        .then(submissions => $scope.pendingSubmissions = submissions.length)
+        .catch(console.error)
+      
+      const loadQuestions = store.get('questions', q => assignmentIds.includes(q.assignmentId) && !q.answer)
+        .then(questions => $scope.unansweredQuestions = questions.length)
+        .catch(console.error)
+      
+      $q.all([ loadSubmissions, loadQuestions ])
+        .then(() => $scope.loading = false)
+    })
+    .catch(console.error)
 }
